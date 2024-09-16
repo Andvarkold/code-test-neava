2693bfc53e505b05ba950a7d398b614607ff81bf
Andvarkold <93447465+Andvarkold@users.noreply.github.com>
2024-09-16 18:33:29 +0200

    WIP parser and dict2xml

--- converter.py
+++ converter.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
-import dict2xml
 import argparse
 from pathlib import Path
+from dict2xml import dict2xml
 
 
 def main(input: str, output: str):
-    input_path = _validate_input(input)
-    output_path = _validate_output(output)
+    input_path = validate_input_argument(input)
+    output_path = validate_output_argument(output)
+
+    with input_path.open() as source, output_path.open("w") as target:
+        target.write("<people>\n")
+        person = []
+        for raw_line in source:
+            line = raw_line.strip()
+            if line[0] == "P" and person:
+                target.write(
+                    dict2xml(parse_person(person), wrap="person", indent="    ") + "\n"
+                )
+                person = [line]
+            else:
+                person.append(line)
+        target.write(
+            dict2xml(parse_person(person), wrap="person", indent="    ") + "\n"
+        )
+        target.write("</people>")
 
 
-def _validate_input(input: str) -> Path:
+def validate_input_argument(input: str) -> Path:
     input_path = Path(input)
     if not input_path.is_absolute():
         raise ValueError(f"Input '{input}' has to be an absolute path!")
@@ -20,7 +37,7 @@ def _validate_input(input: str) -> Path:
     return input_path
 
 
-def _validate_output(output: str) -> Path:
+def validate_output_argument(output: str) -> Path:
     output_path = Path(output)
     if not output_path.is_absolute():
         raise ValueError(f"Output '{output}' has to be an absolute path!")
@@ -28,6 +45,60 @@ def _validate_output(output: str) -> Path:
         raise FileNotFoundError(f"Output '{output}' parent directory does not exists!")
 
     return output_path
+
+
+def parse_person(data: list[str]) -> dict:
+    person_data = {}
+    for line in data:
+        raw_parts = line.split("|")
+        parts = [part.strip() for part in raw_parts]
+
+        match parts[0]:
+            case "P":
+                person_data["firstname"] = parts[1]
+                person_data["lastname"] = parts[2]
+                add_to_family_data = False
+            case "T":
+                temp_data = parse_t(parts[1:])
+
+                if add_to_family_data:
+                    person_data["family"][-1]["phone"] = temp_data
+                else:
+                    person_data["phone"] = temp_data
+            case "A":
+                temp_data = parse_a(parts[1:])
+
+                if add_to_family_data:
+                    person_data["family"][-1]["address"] = temp_data
+                else:
+                    person_data["address"] = temp_data
+            case "F":
+                temp_data = parse_f(parts[1:])
+                if "family" in person_data:
+                    person_data["family"].append(temp_data)
+                else:
+                    person_data["family"] = [temp_data]
+                add_to_family_data = True
+
+    return person_data
+
+
+def parse_t(parts: list[str]) -> dict:
+    data = {"mobile": parts[0], "landline": parts[1]}
+
+    return data
+
+
+def parse_a(parts: list[str]) -> dict:
+    data = {"street": parts[0], "city": parts[1], "postcode": parts[2]}
+
+    return data
+
+
+def parse_f(parts: list[str]) -> dict:
+    data = {"name": parts[0], "born": parts[1]}
+
+    return data
 
 
 if __name__ == "__main__":
